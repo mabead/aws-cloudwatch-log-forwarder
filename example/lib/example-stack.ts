0551610ebd89081ab9a52d412b3fb77deb45ecc1
Maxime Beaudry <mabead@users.noreply.github.com>
2019-10-30 06:27:00 -0400

    create kinesis stream

--- example/lib/example-stack.ts
+++ example/lib/example-stack.ts
@@ -1,9 +1,23 @@
 import cdk = require('@aws-cdk/core');
+import kinesis = require('@aws-cdk/aws-kinesis')
+
+export interface CloudWatchLogForwarderProps {
+  readonly kinesisProps?: kinesis.StreamProps;
+}
+
+export class CloudWatchLogForwarder extends cdk.Construct {
+
+  constructor(scope: cdk.Construct, id: string, props?: CloudWatchLogForwarderProps) {
+    super(scope, id);
+
+    new kinesis.Stream(this, 'Stream', props == undefined ? undefined : props.kinesisProps);
+  }
+}
 
 export class ExampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new CloudWatchLogForwarder(this, "CloudWatchLogForwarder")
   }
 }
