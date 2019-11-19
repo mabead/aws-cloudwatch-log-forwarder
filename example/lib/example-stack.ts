417e46373c94c0837acc649df848d0765409e3e8
Maxime Beaudry <mabead@users.noreply.github.com>
2019-11-18 20:37:41 -0500

    basic lambda

--- example/lib/example-stack.ts
+++ example/lib/example-stack.ts
@@ -1,21 +1,28 @@
-import cdk = require('@aws-cdk/core');
+import core = require('@aws-cdk/core');
 import kinesis = require('@aws-cdk/aws-kinesis')
+import lambda = require('@aws-cdk/aws-lambda');
+import path = require('path');
 
 export interface CloudWatchLogForwarderProps {
   readonly kinesisProps?: kinesis.StreamProps;
 }
 
-export class CloudWatchLogForwarder extends cdk.Construct {
+export class CloudWatchLogForwarder extends core.Construct {
 
-  constructor(scope: cdk.Construct, id: string, props?: CloudWatchLogForwarderProps) {
+  constructor(scope: core.Construct, id: string, props?: CloudWatchLogForwarderProps) {
     super(scope, id);
 
     new kinesis.Stream(this, 'Stream', props?.kinesisProps);
-  }
+    
+    new lambda.Function(this, 'SetExpiry', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'resources', 'SetExpiry.js'))});      
+  } 
 }
 
-export class ExampleStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class ExampleStack extends core.Stack {
+  constructor(scope: core.Construct, id: string, props?: core.StackProps) {
     super(scope, id, props);
 
     new CloudWatchLogForwarder(this, "CloudWatchLogForwarder")
